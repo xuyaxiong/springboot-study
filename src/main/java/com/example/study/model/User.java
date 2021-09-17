0b39c40290992653f8a86bf4f51b24fc077601d8
xuyaxiong <xyxlindy@163.com>
2021-09-17 15:52:15 +0800

    添加Lombok依赖

--- src/main/java/com/example/study/model/User.java
+++ src/main/java/com/example/study/model/User.java
@@ -1,31 +1,10 @@
 package com.example.study.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String name;
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
